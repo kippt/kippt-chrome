6e3753fdb590896ef3ac86768b63ef428d17b915
Bertil Chapuis <bchapuis@agimem.com>
2012-06-21 02:04:46 +0200

    added support for the omnibox

---
+++ kippt_omnibox.js
@@ -0,0 +1,24 @@
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+	if (text.length > 0) {
+		var search = 'https://kippt.com/api/search/clips/?q=' + text;
+		$.getJSON(search, function(data){
+			if (data.objects) {
+				var suggestions = [];
+				for (s in data.objects) {
+					var title = data.objects[s].title;
+					var url = data.objects[s].url;
+					suggestions.push({description: title, content: url});
+				}
+				suggest(suggestions);
+			}
+		});
+
+	}
+	
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+	chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.update(tab.id, {url : text});
+    });
+});
