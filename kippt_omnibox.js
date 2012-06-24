daf1b33afb59f11de78069e74143492d1b61c51e
Jori Lallo <jtlallo@gmail.com>
2012-06-24 15:38:38 -0700

    Better visualization of the search result

--- kippt_omnibox.js
+++ kippt_omnibox.js
@@ -1,13 +1,17 @@
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
     if (text.length > 0) {
-        var search = 'https://kippt.com/api/search/clips/?limit=10&q=' + text;
+        var search = 'https://kippt.com/api/search/clips/?limit=5&q=' + text;
         $.getJSON(search, function(data){
             if (data.objects) {
                 var suggestions = [];
                 for (s in data.objects) {
                     var title = data.objects[s].title;
                     var url = data.objects[s].url;
-                    suggestions.push({description: title, content: url});
+                    var url_domain = data.objects[s].url_domain;
+                    suggestions.push({
+                        description: title+' <dim> - '+url_domain+'</dim>',
+                        content: url
+                    });
                 }
                 suggest(suggestions);
             }
