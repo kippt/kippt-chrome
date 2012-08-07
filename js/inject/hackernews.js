12ce226c70e55cf1aa35b43213aace0c0ee01350
James Potter <james.potter@gmail.com>
2012-08-06 18:09:35 -0700

    HN checks inject opt-out

--- js/inject/hackernews.js
+++ js/inject/hackernews.js
@@ -1,4 +1,5 @@
 $(function() {
+
     var openPopup = function(a, title, url) {
         data = {
             title: title,
@@ -8,29 +9,38 @@ $(function() {
         KipptOpenPopup(data);
     };
 
-    var tdList = $('tr td.subtext');
-    if (tdList.length == 0)
-        return;
-
-    tdList.each(function(i, td) {
-        var aList = $('a', td);
-        if (aList.length != 3)
+    var inject = function() {
+        var tdList = $('tr td.subtext');
+        if (tdList.length == 0)
             return;
 
-        var a = $('<a href="#">kippt</a>');
-        a.on('click', function() {
-            // Get URL and title
-            var tdAbove = td.parentElement.previousSibling.childNodes[2];
-            var aLink = $('a', tdAbove)[0];
-            var title = aLink.innerText;
-            var url = aLink.href;
+        tdList.each(function(i, td) {
+            var aList = $('a', td);
+            if (aList.length != 3)
+                return;
 
-            openPopup(a, title, url);
+            var a = $('<a href="#">kippt</a>');
+            a.on('click', function() {
+                // Get URL and title
+                var tdAbove = td.parentElement.previousSibling.childNodes[2];
+                var aLink = $('a', tdAbove)[0];
+                var title = aLink.innerText;
+                var url = aLink.href;
+
+                openPopup(a, title, url);
+            });
+            
+            // New link at the end
+            var last = aList.last();
+            last.after(a);
+            a.before(' | ');
         });
-        
-        // New link at the end
-        var last = aList.last();
-        last.after(a);
-        a.before(' | ');
+    };
+
+    // Check opt-out status
+    chrome.extension.sendRequest({method: 'getStorage'}, function(response) {
+        var bool = response.storage['inject_hn'];
+        if (bool == 'true' || bool == undefined) inject();
     });
+
 });
