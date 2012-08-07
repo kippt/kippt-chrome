1934b4350f51eb8345e66363c17db21ab96a00da
James Potter <james.potter@gmail.com>
2012-08-06 18:17:24 -0700

    Check opt-out for twitter/google

--- js/inject/googlereader.js
+++ js/inject/googlereader.js
@@ -18,38 +18,46 @@ $(function() {
         return {title: title, url: url};
     };
 
-    $('div#viewer-container').on('click', 'div.expanded', function(event) {
-        // Mouseover fired for child elements so need to find the right div
-        var el = event.target;
+    var inject = function() {
+        $('div#viewer-container').on('click', 'div.expanded', function(event) {
+            // Mouseover fired for child elements so need to find the right div
+            var el = event.target;
 
-        while (!$(el).hasClass('entry')) {
-            if (el == undefined)
+            while (!$(el).hasClass('entry')) {
+                if (el == undefined)
+                    return;
+                el = el.parentElement;
+            }
+            el = $(el);
+            
+            // Actions list
+            var div = $('.entry-actions', $(el));
+
+            // Don't add kippt link again
+            if (hasKipptAction(div))
                 return;
-            el = el.parentElement;
-        }
-        el = $(el);
-        
-        // Actions list
-        var div = $('.entry-actions', $(el));
 
-        // Don't add kippt link again
-        if (hasKipptAction(div))
-            return;
+            // Insert kippt link at the end
+            var a = $('<a href="#">Kippt</a>');
+            a.on('click', function() {
+                var content = getShareContent(el);
+                openPopup(a, content.title, content.url);
+            });
 
-        // Insert kippt link at the end
-        var a = $('<a href="#">Kippt</a>');
-        a.on('click', function() {
-            var content = getShareContent(el);
-            openPopup(a, content.title, content.url);
+            var newAction = $('<span class="action-kippt"></span>');
+            newAction.append(a);
+
+            var c = div.children();
+            var lastAction = c[c.length - 1];
+            $(lastAction).after(newAction);
+
         });
+    };
 
-        var newAction = $('<span class="action-kippt"></span>');
-        newAction.append(a);
-
-        var c = div.children();
-        var lastAction = c[c.length - 1];
-        $(lastAction).after(newAction);
-
+    // Check opt-out status
+    chrome.extension.sendRequest({method: 'getStorage'}, function(response) {
+        var bool = response.storage['inject_google_reader'];
+        if (bool == 'true' || bool == undefined) inject();
     });
 
 });
--- js/inject/twitter.js
+++ js/inject/twitter.js
@@ -39,40 +39,48 @@ $(function() {
         return {url: a.href, title: title};
     };
 
-    $('div.content-main').on('mouseover', '.js-stream-item', function(event) {
-        // Mouseover fired for child elements so need to find the right div
-        var el = event.target;
-        while (!$(el).hasClass('js-stream-item')) {
-            if (el == undefined)
+    var inject = function() {
+        $('div.content-main').on('mouseover', '.js-stream-item', function(event) {
+            // Mouseover fired for child elements so need to find the right div
+            var el = event.target;
+            while (!$(el).hasClass('js-stream-item')) {
+                if (el == undefined)
+                    return;
+                el = el.parentElement;
+            }
+            el = $(el);
+            
+            // Actions list
+            var ul = $('ul.tweet-actions', $(el));
+
+            // Don't add kippt link again
+            if (hasKipptAction(ul))
                 return;
-            el = el.parentElement;
-        }
-        el = $(el);
-        
-        // Actions list
-        var ul = $('ul.tweet-actions', $(el));
 
-        // Don't add kippt link again
-        if (hasKipptAction(ul))
-            return;
+            var content = getShareContent(el);
+            if (content == false)
+                return; // No link in this tweet
 
-        var content = getShareContent(el);
-        if (content == false)
-            return; // No link in this tweet
+            // Insert kippt link at the end
+            var a = $('<a href="#">Kippt</a>');
+            a.on('click', function() {
+                openPopup(a, content.title, content.url);
+            });
 
-        // Insert kippt link at the end
-        var a = $('<a href="#">Kippt</a>');
-        a.on('click', function() {
-            openPopup(a, content.title, content.url);
+            var newAction = $('<li class="action-kippt"></li>');
+            newAction.append(a);
+
+            var c = ul.children();
+            var lastAction = c[c.length - 1];
+            $(lastAction).after(newAction);
+
         });
+    };
 
-        var newAction = $('<li class="action-kippt"></li>');
-        newAction.append(a);
-
-        var c = ul.children();
-        var lastAction = c[c.length - 1];
-        $(lastAction).after(newAction);
-
+    // Check opt-out status
+    chrome.extension.sendRequest({method: 'getStorage'}, function(response) {
+        var bool = response.storage['inject_twitter'];
+        if (bool == 'true' || bool == undefined) inject();
     });
 
 });
